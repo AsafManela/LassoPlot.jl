c1555a7ba6c5dfa805bbccd4edc14e9f9d3d589c
Asaf Manela <AsafManela@users.noreply.github.com>
2018-07-22 22:04:40 -0500

    added tests for windows

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # LassoPlot
 
-[![Build Status](https://travis-ci.org/AsafManela/LassoPlot.jl.svg?branch=master)](https://travis-ci.org/AsafManela/LassoPlot.jl)
+| Linux/MacOS | Windows | Code |
+| --- | --- | --- |
+| [![][travis-img]][travis-url]  | [![][appveyor-img]][appveyor-url]  | [![][coveralls-img]][coveralls-url] |
 
 LassoPlot.jl is a companion package to [Lasso.jl](https://github.com/simonster/Lasso.jl) which plots regularization paths in a similar fashion to the glmnet and gamlr R packages.
 
@@ -36,3 +38,17 @@ To specify other selection criteria use the select=:AICc, :CVmin, or :CV1se.
 Vertical lines are determined by showselectors=[:AICc,:CVmin,:CV1se].
 
 varnames can be used to specify variable names.
+
+[travis-img]: https://travis-ci.org/AsafManela/LassoPlot.jl.svg?branch=master
+[travis-url]: https://travis-ci.org/AsafManela/LassoPlot.jl
+
+[appveyor-img]: https://ci.appveyor.com/api/projects/status/github/AsafManela/LassoPlot.jl?svg=true
+[appveyor-url]: https://ci.appveyor.com/project/AsafManela/lassoplot-jl
+
+[coveralls-img]: https://coveralls.io/repos/AsafManela/LassoPlot.jl/badge.svg?branch=master
+[coveralls-url]: https://coveralls.io/r/AsafManela/LassoPlot.jl?branch=master
+
+[pkg-0.6-img]: http://pkg.julialang.org/badges/LassoPlot_0.6.svg
+[pkg-0.6-url]: http://pkg.julialang.org/?pkg=LassoPlot&ver=0.6
+[pkg-0.7-img]: http://pkg.julialang.org/badges/LassoPlot_0.7.svg
+[pkg-0.7-url]: http://pkg.julialang.org/?pkg=LassoPlot&ver=0.7
--- test/plots.jl
+++ test/plots.jl
@@ -21,7 +21,7 @@ srand(243214)
             γ=params[1,Symbol("fit.gamma")]
 
             # fit julia version
-            glp = fit(GammaLassoPath, X, y, dist, link; γ=γ, λminratio=0.001) #, λ=λ)
+            glp = fit(GammaLassoPath, X, y, dist, link; γ=γ, λminratio=0.001)
 
             # test plots
             p = plot(glp;x=:logλ)
@@ -34,3 +34,30 @@ end
 
 # comment the next line to see plots after test finishes in test/plots/
 rm(plotspath;recursive=true)
+
+#### debugging code ######
+# (family, dist, link) = (("gaussian", Normal(), IdentityLink()), ("binomial", Binomial(), LogitLink()), ("poisson", Poisson(), LogLink()))[1]
+# γ = 0
+#
+# data = CSV.read(joinpath(datapath,"gamlr.$family.data.csv"))
+# y = convert(Vector{Float64},data[:,1])
+# X = convert(Matrix{Float64},data[:,2:end])
+# (n,p) = size(X)
+#
+# fitname = "gamma$γ.pf1"
+# # get gamlr.R params and estimates
+# params = CSV.read(joinpath(datapath,"gamlr.$family.$fitname.params.csv"))
+# fittable = CSV.read(joinpath(datapath,"gamlr.$family.$fitname.fit.csv"))
+# gcoefs = convert(Matrix{Float64},CSV.read(joinpath(datapath,"gamlr.$family.$fitname.coefs.csv")))
+# family = params[1,Symbol("fit.family")]
+# γ=params[1,Symbol("fit.gamma")]
+# # λ = convert(Vector{Float64},fittable[:fit_lambda]) # should be set to nothing evenatually
+#
+# # fit julia version
+# glp = fit(GammaLassoPath, X, y, dist, link; γ=γ, λminratio=0.001) #, λ=λ)
+#
+# # test plots
+# p = plot(glp;x=:logλ)
+# filename = joinpath(plotspath,"$family.$fitname.path.svg")
+# draw(SVG(filename,5inch,5inch),p)
+# @test isfile(filename)
